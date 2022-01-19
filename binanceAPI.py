728ab861a7ed7e22c32c7c65718b85416c783c60
TadiMercury <magara.tadiwa9@gmail.com>
2022-01-19 23:10:39 +0200

    binance API setup

--- binanceAPI.py
+++ binanceAPI.py
@@ -1,4 +1,14 @@
-
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+import pandas as pd
 
 apikey = 'YOURAPIKEY'
 secret = 'YOURAPISECRET'
+pair='XBTZAR'
+
+client = Client(apikey, secret)
+
+tickers = client.get_all_tickers()
+
+trythis = client.get_ticker()
+
+print(trythis)
