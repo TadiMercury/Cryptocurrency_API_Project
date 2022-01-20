7615e9df6e0b2c5dd6ddc1e6591870c53e896ce3
TadiMercury <magara.tadiwa9@gmail.com>
2022-01-20 23:52:45 +0200

    Ammendments 20 Jan

--- binanceAPI.py
+++ binanceAPI.py
@@ -1,14 +1,21 @@
+from symtable import Symbol
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import pandas as pd
 
 apikey = 'YOURAPIKEY'
 secret = 'YOURAPISECRET'
-pair='XBTZAR'
+pair='ETHBTC'
 
 client = Client(apikey, secret)
 
 tickers = client.get_all_tickers()
 
-trythis = client.get_ticker()
+trythis = client.get_ticker(symbol = pair)
 
 print(trythis)
+
+dicte = {}
+dicte = trythis
+
+for i in dicte.keys():
+    print(i)
